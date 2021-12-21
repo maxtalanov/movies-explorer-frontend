6a2f94fffd36e575ff997dc08ccab85a910e1e40
Maxim Talanov <mail@maxtalanov.ru>
2021-12-21 04:54:33 +0300

    fix(style): Исправить ошибки в footor

--- src/components/Navigation/Navigation.js
+++ src/components/Navigation/Navigation.js
@@ -6,7 +6,7 @@ function Navigation({ mod }) {
 
   return(
     <section className={`navigation`}>
-      <a href={socialLinks.instagram} target="_blank" className="navigation__lin hover-opacity" rel="noreferrer">Instagram</a>
+      <a href={socialLinks.instagram} target="_blank" className="navigation__link hover-opacity" rel="noreferrer">Instagram</a>
       <a href={socialLinks.yp} target="_blank" className="navigation__link hover-opacity" rel="noreferrer">Яндекс.Практикум</a>
       <a href={socialLinks.github} target="_blank" className="navigation__link hover-opacity" rel="noreferrer">GitHub</a>
       <a href={socialLinks.fb} target="_blank" className="navigation__link hover-opacity" rel="noreferrer">FaceBook</a>
