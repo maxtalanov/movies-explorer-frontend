0ef235bf5f29e2819d4bbf6281667eaa1d673a18
Maxim Talanov <mail@maxtalanov.ru>
2022-02-04 05:32:44 +0300

    feat(main): Добавить ф-ал к API
    Добавить API на внутренний сервер для взаимодействия с пользователем. Дополнительно в компоненте : "App" добавить ф-ал для обращения к нашему API (сделать точку входа)

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -11,14 +11,78 @@ import Movies from "../Movies/Movies";
 import SavedMovies from "../SavedMovies/SavedMovies";
 import NotFound from "../NotFound/NotFound";
 
+import * as MainAPI from "../../utils/API/MainAPIjs";
+import {userExit} from "../../utils/API/MainAPIjs";
+
 // ф-ый компонент
 function App() {
+
+  function onRegister() {
+    const data = {
+      email: 'email@maxtalanov.ru',
+      password: 'qwerty1234',
+      userName: 'Мистер хрен'
+    }
+    return MainAPI
+      .register(data)
+      .then(res => {
+        console.log(res);
+      })
+      .catch((err) => {
+        console.log(err);
+      })
+  }
+
+  function onLogin() {
+    return MainAPI
+      .login({})
+      .then(res => {
+        console.log(res);
+      })
+      .catch((err) => {
+        console.log(err);
+      })
+  }
+
+  function onUpdateUser() {
+    return MainAPI
+      .updateUser({})
+      .then(res => {
+        console.log(res);
+      })
+      .catch((err) => {
+        console.log(err);
+      })
+  }
+
+  function onExitUser() {
+    return MainAPI
+      .userExit({})
+      .then(res => {
+        console.log(res);
+      })
+      .catch((err) => {
+        console.log(err);
+      })
+  }
+
+  function onGetUser() {
+    return MainAPI
+      .getUser({})
+      .then(res => {
+        console.log(res);
+      })
+      .catch((err) => {
+        console.log(err);
+      })
+  }
+
 
   return (
     <div className="app">
       <Switch>
-
         <Route exact path='/'>
+          onRegister
           <Main/>
         </Route>
 
--- src/utils/API/MainAPIjs.js
+++ src/utils/API/MainAPIjs.js
@@ -1,1 +1,60 @@
-import { checkResponse, BASE_URL_MAIN, BASE_HEADERS_MAIN} from "../constant";
+import {checkResponse, BASE_URL_MAIN, BASE_HEADERS_MAIN} from "../constant";
+
+let BASE_URL = BASE_URL_MAIN;
+
+// РЕГИСТРАЦИЯ USER
+export const getUser = () => {
+
+  return fetch(`${BASE_URL}/users/me`, {
+    method: "GET",
+    credentials: 'include',
+    BASE_HEADERS_MAIN,
+  }).then(checkResponse)
+}
+
+// РЕГИСТРАЦИЯ USER
+export const register = ({email, password, userName}) => {
+  console.log(`API MAIN register | key: ${password}, user: ${email}, name: ${userName}`);
+
+  return fetch(`${BASE_URL}/signup`, {
+    method: "POST",
+    credentials: 'include',
+    BASE_HEADERS_MAIN,
+    body: JSON.stringify({email, password, userName})
+  }).then(checkResponse)
+}
+
+// АВТОРИЗАЦИЯ USER
+export const login = ({email, password}) => {
+  // console.log(`API MAIN login | key: ${password}, user: ${email}`);
+
+  return fetch(`${BASE_URL}/signin`, {
+    method: "POST",
+    credentials: 'include',
+    BASE_HEADERS_MAIN,
+    body: JSON.stringify({email, password})
+  }).then(checkResponse)
+}
+
+// ОБНОВИТЬ ИНФОРМАЦИЮ USER
+export const updateUser = ({email, userName}) => {
+  // console.log(`API MAIN updateUser | name: ${userName}, user: ${email}`);
+
+  return fetch(`${BASE_URL}/users/me`, {
+    method: "PATCH",
+    credentials: 'include',
+    BASE_HEADERS_MAIN,
+    body: JSON.stringify({email, userName})
+  }).then(checkResponse)
+}
+
+// ВЫХОД USER
+export const userExit = () => {
+
+  return fetch(`${BASE_URL}/signout`, {
+    method: "GET",
+    credentials: 'include',
+    BASE_HEADERS_MAIN,
+  }).then(checkResponse)
+}
+
