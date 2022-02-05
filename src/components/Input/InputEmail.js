6d5401a0362a28ef81928d52adfaf1cdb65038f8
Maxim Talanov <mail@maxtalanov.ru>
2022-02-05 10:03:59 +0300

    refactor(input prototype): Изменить прототип инпута
    Разбить компоненту отвечающую за сборку инпутов на соответствующие типы

---
+++ src/components/Input/InputEmail.js
@@ -0,0 +1,29 @@
+import React from "react";
+import "./Input.css";
+
+// ф-ый компонент
+function InputEmail ({ label, placeholder, id, name, value, onChange, errMassage, err,}) {
+
+  return(
+    <>
+      <label className={`form__label`}>{label}</label>
+      <input
+        className={`form__input form__input-err`}
+        placeholder={placeholder}
+        type="email"
+
+        id={id}
+        name={name}
+        value={value}
+        onChange={onChange}
+
+        autoComplete="off"
+        required
+      />
+      <span className={`form__span ${err}`}>{errMassage}</span>
+    </>
+
+  );
+}
+
+export default InputEmail;
---
+++ src/components/Input/InputName.js
@@ -0,0 +1,31 @@
+import React from "react";
+import "./Input.css";
+
+// ф-ый компонент
+function InputName({ label, placeholder, id, name, value, onChange, errMassage, err,}) {
+
+  return(
+    <>
+      <label className={`form__label`}>{label}</label>
+      <input
+        className={`form__input form__input-err`}
+        placeholder={placeholder}
+        type="text"
+
+        id={id}
+        name={name}
+        value={value}
+        onChange={onChange}
+
+        min={2}
+        max={30}
+        autoComplete="off"
+        required
+      />
+      <span className={`form__span ${err}`}>{errMassage}</span>
+    </>
+
+  );
+}
+
+export default InputName;
