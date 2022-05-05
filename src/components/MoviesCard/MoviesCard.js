ca86b7b0464917faf983b6575d95c588adb39884
Maxim Talanov <mail@maxtalanov.ru>
2022-05-05 03:13:56 +0300

    Прокидываем пропсы в компонент movie card

--- src/components/MoviesCard/MoviesCard.js
+++ src/components/MoviesCard/MoviesCard.js
@@ -2,7 +2,7 @@ import React from "react";
 import { timeFormat } from '../../utils/globalMethod/time';
 import './MoviesCard.css';
 
-function MoviesCard({ titleRu, duration, image }) {
+function MoviesCard({movie}) {
   const [modLike, setModLike] = React.useState(''); //Удалить после написания ф-ла
   const movieDuration = timeFormat(duration);
   //Удалить после написания ф-ла
@@ -14,12 +14,12 @@ function MoviesCard({ titleRu, duration, image }) {
     <section className={`card`}>
       <div className="card__container-info">
         <div className="card__movie-item">
-          <h3 className="card__title">{titleRu}</h3>
-          <p className="card__duration">{`${movieDuration.hours}ч ${movieDuration.minute}м`}</p>
+          {/*<h3 className="card__title">{titleRu}</h3>*/}
+          {/*<p className="card__duration">{`${movieDuration.hours}ч ${movieDuration.minute}м`}</p>*/}
         </div>
-        <button className={`card__btn-like ${modLike} hover-opacity`} onClick={likeClick}/>
+        {/*<button className={`card__btn-like ${modLike} hover-opacity`} onClick={likeClick}/>*/}
       </div>
-      <img src={image} alt={`Постер к фильму ${titleRu}`} className="card__img"/>
+      {/*<img src={image} alt={`Постер к фильму ${titleRu}`} className="card__img"/>*/}
     </section>
   );
 }
