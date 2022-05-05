93db2491f2ea3493a23c1d341ed05dac1133c58a
Maxim Talanov <mail@maxtalanov.ru>
2022-05-05 03:12:31 +0300

    Добавить отрисовку компонента с карточкой фильма

--- src/components/MoviesCardList/MoviesCardList.js
+++ src/components/MoviesCardList/MoviesCardList.js
@@ -3,19 +3,18 @@ import './MoviesCardList.css';
 
 import MoviesCard from "../MoviesCard/MoviesCard";
 
-function MoviesCardList({ data }) {
-
-  const renderMoviesCard = data.map((m) => {
-    return (<MoviesCard
-      key={m.movieId}
-      movieData={m}
-    />);
-  });
+function MoviesCardList({ movies, myMovies }) {
 
   return(
     <section className="card-list">
       <div className="card-list__container">
-        {renderMoviesCard}
+        { movies && movies.map(movie =>
+          <MoviesCard
+            key={movie.id}
+            movie={movie}
+          />
+        )
+        }
       </div>
       <button className="card-list__btn-add hover-opacity">Ещё</button>
     </section>
