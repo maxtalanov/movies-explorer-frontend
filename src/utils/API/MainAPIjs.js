8fe83e6f672c7538da64bcf080a50b6ba2dc702a
Maxim Talanov <mail@maxtalanov.ru>
2022-05-06 04:29:43 +0300

    Отрефакторить зону заголовков для запроса

--- src/utils/API/MainAPIjs.js
+++ src/utils/API/MainAPIjs.js
@@ -1,6 +1,6 @@
-import { checkResponse, BASE_URL_MAIN, BASE_HEADERS_MAIN, BASE_URL_MOVIES } from "../constant";
-
+import { checkResponse, BASE_URL_MAIN, BASE_HEADERS_MAIN } from "../constant";
 let BASE_URL = BASE_URL_MAIN;
+let headers = BASE_HEADERS_MAIN;
 
 // РЕГИСТРАЦИЯ USER
 export const getUser = () => {
@@ -8,7 +8,7 @@ export const getUser = () => {
   return fetch(`${BASE_URL}/users/me`, {
     method: "GET",
     credentials: 'include',
-    headers: BASE_HEADERS_MAIN,
+    headers,
   }).then(checkResponse)
 }
 
@@ -18,7 +18,7 @@ export const register = ({email, password, userName}) => {
   return fetch(`${BASE_URL}/signup`, {
     method: "POST",
     credentials: 'include',
-    headers: BASE_HEADERS_MAIN,
+    headers,
     body: JSON.stringify({
       email: email,
       password: password,
@@ -33,7 +33,7 @@ export const login = ({email, password}) => {
   return fetch(`${BASE_URL}/signin`, {
     method: "POST",
     credentials: 'include',
-    headers: BASE_HEADERS_MAIN,
+    headers,
     body: JSON.stringify({email, password})
   }).then(checkResponse)
 }
@@ -44,7 +44,7 @@ export const updateUser = ({email, userName}) => {
   return fetch(`${BASE_URL}/users/me`, {
     method: "PATCH",
     credentials: 'include',
-    headers: BASE_HEADERS_MAIN,
+    headers,
     body: JSON.stringify({email, name: userName })
   }).then(checkResponse)
 }
@@ -55,7 +55,7 @@ export const userExit = () => {
   return fetch(`${BASE_URL}/signout`, {
     method: "POST",
     credentials: 'include',
-    headers: BASE_HEADERS_MAIN,
+    headers,
   }).then(checkResponse)
 }
 
@@ -65,41 +65,28 @@ export const getMyMovies = () => {
   return fetch(`${BASE_URL}/movies`, {
     method: "GET",
     credentials: 'include',
-    headers: BASE_HEADERS_MAIN,
+    headers,
   }).then(checkResponse)
 }
 
 // СОЗДАТЬ ФИЛЬМ
 export const saveMovie = (movie) => {
-
+  console.log(movie)
   return fetch(`${BASE_URL}/movies`, {
     method: "POST",
     credentials: 'include',
-    headers: BASE_HEADERS_MAIN,
-    body: JSON.stringify({
-      country: movie.country,
-      director: movie.director,
-      duration: movie.duration,
-      year: movie.year,
-      description: movie.description,
-      image: `${BASE_URL_MOVIES}${movie.image.url}`,
-      trailer: movie.trailerLink,
-      thumbnail: `${BASE_URL_MOVIES}${movie.image.url}`,
-      movieId: movie.id,
-      nameRU: movie.nameRU,
-      nameEN: movie.nameEN,
-    })
+    headers,
+    body: JSON.stringify(movie)
   }).then(checkResponse)
 }
 
 // УДАЛИТЬ ФИЛЬМ
 export const removeMovie = (movieId) => {
 
-
   return fetch(`${BASE_URL}/movies/${movieId}`, {
     method: "DELETE",
     credentials: 'include',
-    headers: BASE_HEADERS_MAIN,
+    headers,
   }).then(checkResponse)
 }
 
