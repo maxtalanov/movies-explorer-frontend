82bb3be44ff43be6a46e4db3a14deed11041e32c
Maxim Talanov <mail@maxtalanov.ru>
2022-05-09 04:18:58 +0300

    Добавить обработчик клика для кнопки ещё

--- src/components/MoviesCardList/MoviesCardList.js
+++ src/components/MoviesCardList/MoviesCardList.js
@@ -5,12 +5,16 @@ import MoviesCard from "../MoviesCard/MoviesCard";
 
 function MoviesCardList({ children }) {
 
+  const handleClickMore = () => {
+    console.log('Добавить еще')
+  }
+
   return(
     <section className="card-list">
       <div className="card-list__container">
         { children }
       </div>
-      <button className="card-list__btn-add hover-opacity">Ещё</button>
+      <button className="card-list__btn-add hover-opacity" onClick={handleClickMore}>Ещё</button>
     </section>
   );
 }
