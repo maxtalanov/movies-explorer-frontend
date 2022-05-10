b5c29e237de06b5aa75d183e58284ba59833644c
Maxim Talanov <mail@maxtalanov.ru>
2022-05-10 06:22:25 +0300

    Исправить отрисовку компонета SearchForm

--- src/components/SearchForm/SearchForm.js
+++ src/components/SearchForm/SearchForm.js
@@ -5,16 +5,33 @@ import Checkbox from "../Checkbox/Checkbox";
 
 function SearchForm() {
 
-  return(
+  return (
     <section className="search-form">
       <div className="search-form__container">
-        <input type="text" className="search-form__input" placeholder="Фильмы"/>
-        <span className="search-for__span" />
+        <form className="search-form__form" onSubmit={handleSubmit}>
+          <fieldset className="search-form__fieldset">
+            <input
+              className="search-form__input"
+              type="search"
+              placeholder="Фильмы"
+              name="search"
+              value={searchForm.search}
+              onChange={handleChange}
+            />
+            <button className="search-form__submit-btn hover-opacity" type="submit"/>
+          </fieldset>
+
+          <fieldset className="search-form__fieldset">
+            <Checkbox
+              label="Короткометражки"
+              name="shortFilm"
+              checked={searchForm.shortFilm}
+              handleChange={handleChange}
+            />
+          </fieldset>
+        </form>
+        <span className="search-form__line"/>
       </div>
-
-      <Checkbox label="Короткометражки" />
-
-      <span className="search-form__line" />
     </section>
   );
 }
