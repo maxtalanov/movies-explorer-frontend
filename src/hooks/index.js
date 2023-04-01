b9bb31e3ef71ee30ee904f788f5870482c95104a
Maxim Talanov <mail@maxtalanov.ru>
2023-04-01 03:43:57 +0300

    feat(hook #4): Добавить индексный ф-ил hook

---
+++ src/hooks/index.js
@@ -0,0 +1,10 @@
+import useRenderCard from "./useRenderCard/useRenderCard";
+import useWindowDimensions from "./useWindowsDimension/windowDimensions";
+import useStateCash from "./useStateCash/useStateCash";
+
+
+export {
+    useRenderCard, 
+    useWindowDimensions,
+    useStateCash,
+};
