b385574b45c16a2c07bdf7d7d260bcc1c7368bd6
Maxim Talanov <mail@maxtalanov.ru>
2023-04-01 05:10:29 +0300

    refactor(app #6): В компоненте app изменить импорты на alias
      Login,
      Main,
      Movies,
      NotFound,
      ProtectedRoute,
      Profile,
      Register,
      SavedMovies

--- src/components/index.js
+++ src/components/index.js
@@ -1,6 +1,35 @@
 // Компоненты: 'A'
 import App from './App/App'
 
+// Компоненты: 'B'
+// Компоненты: 'C'
+// Компоненты: 'F'
+// Компоненты: 'H'
+// Компоненты: 'I'
+// Компоненты: 'l'
+import Login from './Login/Login'
+// Компоненты: 'M'
+import Main from './Main/Main'
+import Movies from './Movies/Movies'
+// Компоненты: 'N'
+import NotFound from './NotFound/NotFound'
+// Компоненты: 'P'
+import ProtectedRoute from './ProtectedRoute/ProtectedRoute'
+import Profile from './Profile/Profile'
+// Компоненты: 'R'
+import Register from './Register/Register'
+// Компоненты: 'S'
+import SavedMovies from './SavedMovies/SavedMovies'
+// Компоненты: 'T'
+// Компоненты: 'W'
 export {
-    App
+    App,
+    Login,
+    Main,
+    Movies,
+    NotFound,
+    ProtectedRoute,
+    Profile,
+    Register,
+    SavedMovies
 }
