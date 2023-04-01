c0ab884ec80fd8004ea6dd71a3f16259d62cf2db
Maxim Talanov <mail@maxtalanov.ru>
2023-04-01 04:41:35 +0300

    feat(component #1): Добавить индексный фаил

--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom'
+import { App } from 'components'
 
 import './index.css';
-import App from './components/App/App';
 
 ReactDOM.render(
   <React.StrictMode>
