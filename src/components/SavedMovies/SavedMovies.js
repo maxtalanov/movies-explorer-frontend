9cede74cbb6e080c58cf9e78e8e2208a83eec239
Maxim Talanov <mail@maxtalanov.ru>
2023-04-01 03:48:21 +0300

    refactor(s movies #1): Удалить не востребованные импорты

--- src/components/SavedMovies/SavedMovies.js
+++ src/components/SavedMovies/SavedMovies.js
@@ -1,4 +1,4 @@
-import React, {useEffect, useState} from "react";
+import React from "react";
 import NavMenuHeader from "../NavMenuHeader/NavMenuHeader";
 import Header from "../Header/Header";
 import SearchForm from "../SearchForm/SearchForm";
