21ebf126cb15938d46ce67f119669e9acc4b147a
Maxim Talanov <mail@maxtalanov.ru>
2023-04-26 22:09:10 +0300

    refactor(protected router): Удалить комментарий из кода

--- src/components/ProtectedRoute/ProtectedRoute.js
+++ src/components/ProtectedRoute/ProtectedRoute.js
@@ -2,8 +2,6 @@ import React from "react";
 import { Route, Redirect } from "react-router-dom";
 import { ROUTERS } from 'routers'
 
-// этот компонент принимает другой компонент в качестве пропса
-// он также может взять неограниченное число пропсов и передать их новому компоненту
 const ProtectedRoute = ({ component: Component, ...props }) => {
 
   return (
