a1d7f6efe1b8b9388ad15b1fac12f5dfaa4e5369
Maxim Talanov <mail@maxtalanov.ru>
2023-04-27 04:50:36 +0300

    refactor(protected): Добавить прелоуде

--- src/components/ProtectedRoute/ProtectedRoute.js
+++ src/components/ProtectedRoute/ProtectedRoute.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { Route, Redirect } from "react-router-dom";
+import { Preloader } from "components";
 import { ROUTERS } from 'routers'
 
 const ProtectedRoute = ({ component: Component, ...props }) => {
@@ -8,7 +9,7 @@ const ProtectedRoute = ({ component: Component, ...props }) => {
   return (
     <Route>
       {() =>
-        isTokenCheck ? console.log('Работает прелоадер') : isLoggedIn 
+        isTokenCheck ? <Preloader massage='Идет проверка авторизации' /> : isLoggedIn 
         ? <Component {...props} /> 
         : <Redirect to={ROUTERS.DEFAULT} />
       }
